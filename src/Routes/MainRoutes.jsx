6e8656dac00b31109b112cad9e12c399105d20b9
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2023-11-04 19:34:02 +0600

    error page done

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -3,26 +3,29 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import PageError from "../Pages/PageError/PageError";
 
 const MainRoutes = createBrowserRouter([
     {
         path:'/',
         element:<MainLayout/>,
+        errorElement:<PageError/>,
         children:[
             {
                 path:'/',
                 element:<Home/>
             },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'register',
+                element:<Register/>
+            }
         ]
     },
-    {
-        path:'/login',
-        element:<Login/>
-    },
-    {
-        path:'register',
-        element:<Register/>
-    }
+   
 ])
 
 
